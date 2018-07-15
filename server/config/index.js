72e55313102fdfd27cbe8e918e72c1432512b4cb
mariotury <mariotury@outlook.com>
2018-07-15 15:41:11 +0300

    Adding connection to mlab test db

--- server/config/index.js
+++ server/config/index.js
@@ -1,8 +1,9 @@
 // Config object for the database
 // Deployment and test database
 const config ={
-	db: 'mongodb://127.0.0.1/pixel_app_db',
-	testDb: 'mongodb://127.0.0.1/pixel_app_db_test'
+	//db: 'mongodb://127.0.0.1/pixel_app_db',
+	testDb: 'mongodb://127.0.0.1/pixel_app_db_test',
+	db: 'mongodb://pixel:pixeltest1@ds121251.mlab.com:21251/pixel_test'
 };
 
 module.exports = config;
