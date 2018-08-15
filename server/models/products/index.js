54a96e7515a0f66dec33f01218ab79f1288d78b4
mariotury <mariotury@outlook.com>
2018-08-15 17:04:26 +0300

    New change to products model names

--- server/models/products/index.js
+++ server/models/products/index.js
@@ -3,16 +3,16 @@ const Schema = mongoose.Schema;
 
 //Define ProductSchema
 const ProductSchema = new Schema({
-	product_name: String,
+	productName: String,
 	sku: String,
 	supplier: String,
 	description: String,
-	cost_px: Number,
-	selling_px: Number,
-	stock_on_hand: Number,
+	costPx: Number,
+	sellingPx: Number,
+	stockOnHand: Number,
 	category: String,
 	comment: String,
-	approved_by: String,
+	approvedBy: String,
 	status: {type: Boolean, default: true},
 	createdAt: {type: Date, default: Date.now}
 });
